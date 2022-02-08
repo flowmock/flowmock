564f19e918849a67aa62b412c497c1e4e84cf26d
Vorn Mom <vorn@moonberrytech.com>
2022-02-08 08:37:58 -0500

    Forgot to commit new file.

---
+++ src/FlowMock/FlowMock.Engine/Models/Rules/RequestBodyData.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.Json.Serialization;
+
+namespace FlowMock.Engine.Models.Rules
+{
+    public class RequestBodyData
+    {
+        [JsonPropertyName("op")]
+        public string Op { get; set; }
+
+        [JsonPropertyName("text")]
+        public string Text { get; set; }
+    }
+}
